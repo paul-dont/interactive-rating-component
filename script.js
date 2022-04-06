c0793aa0c8d3d0b5b180ed69a54942e60ee20cc8
Pavlos <pauldontas@gmail.com>
2022-04-06 12:13:42 +0300

    disabled btn if no selection

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 const btnSelect = document.querySelectorAll('.btn--select');
-console.log(btnSelect);
-const btnSelected = Array.from(btnSelect);
-console.log(btnSelected);
 const submitBtn = document.querySelector('.btn');
 const card = document.querySelector('.card');
 const cardThanks = document.querySelector('.card__thanks');
-const ratin = document.querySelector('.rating');
+const rating = document.querySelector('.rating');
 let getNum;
-calcRating();
+let selection = false;
 function calcRating() {
-  btnSelected.forEach(el => {
+  btnSelect.forEach(el => {
     el.addEventListener('click', function (e) {
       el.classList.toggle('btn--selected');
       getNum = el.innerText;
+      selection = true;
     });
   });
 }
+calcRating();
+
 submitBtn.addEventListener('click', function () {
-  card.classList.add('hidden');
-  cardThanks.classList.remove('hidden');
-  ratin.textContent = getNum;
+  if (selection) {
+    card.classList.add('hidden');
+    cardThanks.classList.remove('hidden');
+    rating.textContent = getNum;
+  }
 });
